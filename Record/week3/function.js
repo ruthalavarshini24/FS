01e222c703066f5b26b967e0cb8cf18dfedfeca7
ruthalavarshini24 <ruthalavarshini.24.csm@anits.edu.in>
2026-08-30 15:23:58 +0530

    Update function.js

--- Record/week3/function.js
+++ Record/week3/function.js
@@ -22,12 +22,4 @@ food.shift();
 console.log(" array is: ",food);
 const extend=[...food,"chicken curry","pulav"];
 console.log(extend);
-//functions
-function greet(a,b){
-    result=a+b;
-}
-console.log(greet(8,9));
-console.log(result);
-//arrow functions
-const square = n => n * n;
-console.log(square(4));
+
